7aabfd840e365d0ca8c1502fbdeced9950c7666a
Uwminu <rayigo98@gmail.com>
2022-03-14 16:32:01 +0800

    --no-edit

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   MONGODB:
-    'mongodb+srv://rayigo98:ray12345678@ray.var1c.mongodb.net/unshift-scheduler?retryWrites=true&w=majority',
+    'mongodb+srv://rayigo98:ray12345678@ray.var1c.mongodb.net/unshift-scheduler-pg?retryWrites=true&w=majority',
   SECRET_KEY: 'key',
   PUBSUB_SECRET_KEY: 'pubsub_key',
 }
